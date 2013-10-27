a3bad5a00c97db95978ddb131b187841a1de2318
Tim Mickelson <tmickelson@cubeyou.com>
2013-10-27 08:54:49 +0100

    Total restructure of project, need to make functional

---
+++ ws-beans/src/main/java/eu/mickelson/bg/ws/beans/TaskBean.java
@@ -0,0 +1,15 @@
+package eu.mickelson.bg.ws.beans;
+
+public class TaskBean {
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}  // end public class TaskBean
